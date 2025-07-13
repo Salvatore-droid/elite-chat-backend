c2c0662d8cd5b580515b5da6cdcc7b0b4b555648
Salvatore-droid <geniusokwemba53@gmail.com>
2025-07-13 14:29:50 +0300

    switched from asgi to wsgi

--- chatapp/wsgi.py
+++ chatapp/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
 
-application = get_wsgi_application()
+application = ProtocolTypeRouter({
+    "http": get_wsgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
